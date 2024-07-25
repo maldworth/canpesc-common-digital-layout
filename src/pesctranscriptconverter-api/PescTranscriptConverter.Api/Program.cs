66814d63cccb60fa27d992d2e2ed53ca0c2e5504
Michael Aldworth <aldworth@ocas.ca>
2024-07-25 15:33:06 -0400

    Use Proper class in program.cs with main method so docker build can find it

--- src/pesctranscriptconverter-api/PescTranscriptConverter.Api/Program.cs
+++ src/pesctranscriptconverter-api/PescTranscriptConverter.Api/Program.cs
@@ -3,74 +3,78 @@ using System.Text.Json.Serialization;
 using FastEndpoints;
 using FastEndpoints.ClientGen;
 using FastEndpoints.Swagger;
-using PescTranscriptConverter.Api;
 using PescTranscriptConverter.Api.HostedServices;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace PescTranscriptConverter.Api;
 
-// override XmlException: Resolving of external URIs was prohibited.
-AppContext.SetSwitch("Switch.System.Xml.AllowDefaultResolver", true);
-
-// Add service defaults & Aspire components.
-builder.AddServiceDefaults();
-
-// Add services to the container.
-builder.Services.AddProblemDetails();
-builder.Services.AddFastEndpoints()
-    .SwaggerDocument(o =>
-    {
-        o.DocumentSettings = s =>
-        {
-            s.DocumentName = "latest";
-            s.Title = "PESC Transcript Converter API";
-            s.Version = "v1";
-        };
-        o.AutoTagPathSegmentIndex = 0; // disable auto-tagging
-        o.ShortSchemaNames = true;
-        o.RemoveEmptyRequestSchema = true;
-    })
-    .ConfigureHttpJsonOptions(o =>
-    {
-        o.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
-    });
-
-builder.AddCustomFluentStorage();
-builder.AddCustomApplicationServices();
-builder.Services.AddHostedService<XsltLoadWorker>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseExceptionHandler();
-
-app.MapDefaultEndpoints();
-
-app.UseFastEndpoints(c =>
+public class Program
 {
-    c.Endpoints.ShortNames = true;
-    c.Errors.UseProblemDetails();
-    c.Serializer.Options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-});
-app.UseSwaggerGen();
+    public static async Task<int> Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-await app.GenerateClientsAndExitAsync(
-                    "latest", app.Configuration["NSwagGeneratorOutputPath"] ?? Directory.GetCurrentDirectory(),
-                    csSettings: c =>
-                    {
-                        c.ClassName = "PescTranscriptConverterClient";
-                        c.CSharpGeneratorSettings.Namespace = "PescTranscriptConverter.Tests";
-                        c.CSharpGeneratorSettings.JsonLibrary = NJsonSchema.CodeGeneration.CSharp.CSharpJsonLibrary.SystemTextJson;
+        // override XmlException: Resolving of external URIs was prohibited.
+        AppContext.SetSwitch("Switch.System.Xml.AllowDefaultResolver", true);
 
-                        c.GenerateClientInterfaces = true;
-                        c.GenerateOptionalParameters = true;
-                        c.UseBaseUrl = false;
-                    },
-                    tsSettings: null
-                );
+        // Add service defaults & Aspire components.
+        builder.AddServiceDefaults();
 
-app.Run();
+        // Add services to the container.
+        builder.Services.AddProblemDetails();
+        builder.Services.AddFastEndpoints()
+            .SwaggerDocument(o =>
+            {
+                o.DocumentSettings = s =>
+                {
+                    s.DocumentName = "latest";
+                    s.Title = "PESC Transcript Converter API";
+                    s.Version = "v1";
+                };
+                o.AutoTagPathSegmentIndex = 0; // disable auto-tagging
+                o.ShortSchemaNames = true;
+                o.RemoveEmptyRequestSchema = true;
+            })
+            .ConfigureHttpJsonOptions(o =>
+            {
+                o.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            });
 
-/// <summary>
-/// Make the implicit Program class public so test projects can access it
-/// </summary>
-public partial class Program { }
+        builder.AddCustomFluentStorage();
+        builder.AddCustomApplicationServices();
+        builder.Services.AddHostedService<XsltLoadWorker>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseExceptionHandler();
+
+        app.MapDefaultEndpoints();
+
+        app.UseFastEndpoints(c =>
+        {
+            c.Endpoints.ShortNames = true;
+            c.Errors.UseProblemDetails();
+            c.Serializer.Options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        });
+        app.UseSwaggerGen();
+
+        await app.GenerateClientsAndExitAsync(
+                            "latest", app.Configuration["NSwagGeneratorOutputPath"] ?? Directory.GetCurrentDirectory(),
+                            csSettings: c =>
+                            {
+                                c.ClassName = "PescTranscriptConverterClient";
+                                c.CSharpGeneratorSettings.Namespace = "PescTranscriptConverter.Tests";
+                                c.CSharpGeneratorSettings.JsonLibrary = NJsonSchema.CodeGeneration.CSharp.CSharpJsonLibrary.SystemTextJson;
+
+                                c.GenerateClientInterfaces = true;
+                                c.GenerateOptionalParameters = true;
+                                c.UseBaseUrl = false;
+                            },
+                            tsSettings: null
+                        );
+
+        await app.RunAsync();
+
+        return 0;
+    }
+}
